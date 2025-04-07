ea9846e0de124f7a947eaf5bb5c66e30dbad0461
Franklin <franklinpereira98@hotmail.com>
2025-04-07 15:16:14 -0300

    Styles general changes

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "March 2025 Developer’s Note",
+    "description": "The second developer’s notes of this year are out, acknowledging the shortcoming of resource acquisition and providing more opportunities to aquire more. Pass costume reruns will be properly starting in April, now with a proper plan for the future. And most important: Union Raid changes!",
+    "photoCover":"https://i0.wp.com/nikke.gg/wp-content/uploads/Developers-Note-1.jpg"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "Nikke 2.5 Anniversary Livestream",
+    "description": "The 2.5 anniversary will come after Crusts banner. Make sure to tune into the stream for a sneak-peak of what’s to come as well as some CD-Keys!",
+    "photoCover":"https://i0.wp.com/nikke.gg/wp-content/uploads/Bild_2025-04-07_084619749.png"
+  },
+  {
+    "id":"3",
+    "title":"Stellar Blade x Goddess of Victory – Nikke DLC Collaboration Coming in June",
+    "description": "Stellar Blade, SHIFT UP’s other game on PlayStation, is finally coming to PC along with a two-way collaboration with Nikke! The Stellar Blade x Nikke DLC will be released in June 2025 and can now be wishlisted on Steam. The collaboration has been teased a while back and is likely to feature the likes of Eve, Lily, Taki, and Raven in Nikke.",
+    "photoCover": "https://i0.wp.com/nikke.gg/wp-content/uploads/Stellar-Blade-x-Goddess-of-Victory-Nikke-DLC-Collaboration-Coming-in-June.jpg"
+  },
+  {
+    "id":"4",
+    "title": "April 10 Update Patch Notes",
+    "description": "Champions Arena is finally getting its beta season and OLD TALES will be added to the Archives! Siren is getting a Side Story and a new smol Nikke from the current event will also instantly be added… when K 🙁",
+    "photoCover":"https://i0.wp.com/nikke.gg/wp-content/uploads/Notice_Emu.jpg"
   }
 ]
